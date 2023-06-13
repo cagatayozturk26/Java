c55a1762e68ed0be8a25ce4248559732efe7b36e
cagatayozturk26 <av.cagatayozturk@gmail.com>
2023-06-13 11:21:24 +0300

    Update Taksimetre.java

--- Project4/src/Taksimetre.java
+++ Project4/src/Taksimetre.java
@@ -4,17 +4,16 @@ public class Taksimetre {
     public static void main(String[] args) {
 
         int yol;
-        double ucret;
-        double sonucret;
+        double ucret , perKM = 2.20;
 
         Scanner km = new Scanner(System.in);
         System.out.print("Alınan toplam yolu giriniz:");
         yol = km.nextInt();
 
-        ucret = 10 + (yol * 2.20);
+        ucret = 10 + (yol * perKM);
 
-        sonucret = ucret<20 ? 20 : ucret;
+        ucret = ucret<20 ? 20 : ucret;
 
-        System.out.println("Ödenmesi gereken ücret :" + sonucret);
+        System.out.println("Ödenmesi gereken ücret :" + ucret);
     }
 }
