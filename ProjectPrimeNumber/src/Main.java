fe742a78e8dfc41033866eb142f6ce699db8d15e
cagatayozturk26 <av.cagatayozturk@gmail.com>
2023-06-21 14:05:55 +0300

    Asal Sayı Projesi eklemeler

--- ProjectPrimeNumber/src/Main.java
+++ ProjectPrimeNumber/src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int num1, num2, toplam = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Küçük sayıyı giriniz: ");
+        num1 = input.nextInt();
+        System.out.print("Büyük sayıyı giriniz: ");
+        num2 = input.nextInt();
 
-        System.out.print("1 ile 100 arasındaki asal sayılar: ");
-        for (int i = 2; i <= 100; i++) {
+        System.out.print(num1 + " ile " + num2 + " arasındaki asal sayılar: ");
+        for (int i = num1; i <= num2; i++) {
             int count = 0;
             for (int x = 1; x <= i; x++) {
                 if (i % x == 0) {
@@ -10,8 +18,10 @@ public class Main {
                 }
             }
             if (count == 2) {
-                System.out.print(i + ", ");
+                System.out.print(i + "\n");
+                toplam++;
             }
         }
+        System.out.print("\n" + num1 + " ile " + num2 + " arasındaki asal sayılar toplam " + toplam + " tanedir.");
     }
 }
